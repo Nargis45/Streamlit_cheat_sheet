8316b81e8829538131bde1895af9b03352ef7ac2
Nargis Nasreen <60965420+Nargis45@users.noreply.github.com>
2024-10-31 13:27:36 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -18,4 +18,13 @@ st.markdown(
 listTabs = ["Cheat Sheet", "Designs"]
 tab1, tab2= st.tabs(listTabs)
 
+with tab1:
+    col1, col2, col3= st.columns([4,5,4])
+    with col1:
+        st.write('Install and Import')
+        st.code("""$ pip install streamlit
+import streamlit as st""")
+        st.write('Run streamlit file')
+        st.code("""streamlit run filename.py""")
+
 
