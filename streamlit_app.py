b36807f40b48648795a1505d1d54b945cfe75c19
Nargis Nasreen <60965420+Nargis45@users.noreply.github.com>
2024-10-31 15:24:01 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1125,7 +1125,7 @@ with tab3:
                 scores["Introvert"] += 1
     
     # Streamlit app layout
-    st.title("🌟 Personality Quiz: Are You an Extrovert, Introvert, or Ambivert?")
+    st.title("🌟 Personality Quiz: Are You an Extrovert, Introvert, or Ambivert?🌟")
     st.header("Answer the following questions:")
     
     responses = []
